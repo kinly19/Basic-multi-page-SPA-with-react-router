7b6249c6cc96871f5e5a8c735d8476e84e1879af
unknown <kinchoily@hotmail.com>
2021-11-09 20:39:10 +0000

    Products.js - use map to return a list of li

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,18 @@
-const Products = () => {
+import { Link } from 'react-router-dom';
+
+const Products = (props) => {
+
   return (
-    <div>
+    <section>
       <h1>The Products Page</h1>
-    </div>
+      <ul>
+        {props.list.map((product) => (
+          <li>
+            <Link to={`/products/${product.id}`}> {product.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
