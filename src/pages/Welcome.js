671f61653a017e636d198d7a74774c78a6fe4abb
unknown <kinchoily@hotmail.com>
2021-11-10 18:03:29 +0000

    Welcome.js - setup nested Route to greet new users

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,15 @@
+import { Route } from 'react-router-dom';
+
 const Welcome = () => {
   return (
-    <h1>The Welcome Page </h1>
+    <section>
+      <h1>The Welcome Page </h1>
+        {/* we can nest Routes inside of other components */}
+      <Route path='/welcome/new-user'>
+        <p>Welcome New User</p>
+        {/* or output another component*/}
+      </Route>
+    </section>
   );
 };
 
